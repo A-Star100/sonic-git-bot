0588ae8d0fdee083675c3d0693d0cda8eef11a1c
Anirudh Sevugan <sonicforces207@gmail.com>
2025-07-10 13:58:05 -0500

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,20 @@ module.exports = (app) => {
 
   app.on("issues.opened", async (context) => {
     return context.octokit.issues.createComment(
-      context.issue({ body: "Hello, World!" })
+      context.issue({ body: "Hey there! Thanks for opening your issue! We'll review it faster than the speed of sound!" })
     );
+  });
+
+  // Pull Requests opened
+  app.on("pull_request.opened", async (context) => {
+    const pr = context.payload.pull_request;
+    const sender = context.payload.sender.login;
+
+    return context.octokit.issues.createComment({
+      owner: context.payload.repository.owner.login,
+      repo: context.payload.repository.name,
+      issue_number: pr.number,
+      body: `Thanks for the PR, @${sender}! We'll review it soon! Gotta go fast! 🚀`,
+    });
   });
 };
